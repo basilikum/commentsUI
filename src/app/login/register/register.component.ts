e95dafcf34a190420f7ebe0ec4262db4c7a43f6b
Bastian Krüger <bastian.krueger@kit.edu>
2017-04-25 14:29:00 +0200

    temp

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/toPromise';
 
 import { AuthService } from '../../core/auth/auth.service';
 
@@ -13,6 +15,8 @@ import { AuthService } from '../../core/auth/auth.service';
 export class RegisterComponent implements OnInit {
 
     registerForm: FormGroup;
+
+    private timer: any;
 
     constructor(
         private auth: AuthService,
@@ -46,7 +50,21 @@ export class RegisterComponent implements OnInit {
     }
 
     private isUsernameUnique(control: FormControl) {
-        return this.auth.userExists(control.value).map(exists => !exists);
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
+        const q = new Promise((resolve, reject) => {
+            this.timer = setTimeout(() => {
+                this.auth.userExists(control.value).subscribe((exists) => {
+                    if (exists) {
+                        resolve({ userUnique: true});
+                    } else {
+                        resolve(null);
+                    }
+                });
+            }, 500);
+        });
+        return q;
     }
 
     private initForm() {
