f0e9ea65d2c3c42aa0686bf3a70c8a2f94a185e2
Bastian Krüger <bastian.krueger@kit.edu>
2017-05-25 23:23:45 +0200

    temp

--- src/app/shared/cropper.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CropperDirective } from './cropper.directive';
-
-describe('CropperDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CropperDirective();
-    expect(directive).toBeTruthy();
-  });
-});
